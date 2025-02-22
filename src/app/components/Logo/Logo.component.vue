db260b4680de8ab939423a5af9b1dfabb4f24fba
GwerhDev <gwerh.dev@gmail.com>
2025-02-22 14:51:35 -0300

    styling & adding awesome font icon

--- src/app/components/Logo/Logo.component.vue
+++ src/app/components/Logo/Logo.component.vue
@@ -11,6 +11,10 @@ defineProps({
 <template>
   <router-link class="d-flex align-cent logotype" to='/'>
     <img src="../../../assets/svg/laruinarecords-logo.svg" @click="closeMenu()" :width="width || '50'" alt="">
-    <p class="type">La Ruina</p>
-  </router-link>
+    <span class="type-container">
+      <p>LA RUINA</p>
+      <p class="subtitle">
+        <small>RECORDS</small>
+      </p>
+    </span>  </router-link>
 </template>
